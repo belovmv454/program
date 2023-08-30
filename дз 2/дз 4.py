f76a50bdac811a28712507ff78c28b63ad57441d
belovmv454 <belov_m_v@mail.ru>
2023-08-30 15:15:14 +0300

    Ускорил процесс выполнения кода

--- дз 2/дз 4.py
+++ дз 2/дз 4.py
@@ -1,15 +1,15 @@
-primes = [1, 2, 3]
-for i in range(4, int((10 ** 7) ** 0.5)):
+primes = [2, 3, 5, 7]
+for i in range(9, int((10 ** 7) ** 0.5), 2):
     c = 0
     if i > (10 ** 7) ** 0.5:
         break
     else:
-        for j in range(2, i):
+        for j in range(2, int(i ** 0.5) + 1):
             if i % j == 0:
                 break
             else:
                 c += 1
-        if c == i - 2:
+        if c == int(i ** 0.5) + 1 - 2:
             primes.append(i)
 print(primes)
         
